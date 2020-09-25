2011bf4d1b3b400b6b9bc493288cca9c75f7faaf
Jarquevious <jarquevious@gmail.com>
2020-09-25 16:24:45 -0700

    add item and show item functions working

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -42,3 +42,21 @@ for (let i=0; i<data.length; ++i) {
     itemsContainer.appendChild(newDiv)
 }
 
+    const cart = [ ]
+
+    function addItem(name, price) {
+        const item ={name: name, price: price, qty: 1}
+        cart.push(name)
+    }
+
+    function showItems() {
+        console.log(`You have ${cart.length} items in your cart`)
+    }
+    
+    
+    addItem('paper', 1.00)
+    showItems()
+    addItem('pencil', .25)
+    showItems()
+    addItem('pen', .50)
+    showItems()
